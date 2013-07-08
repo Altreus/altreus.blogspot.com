eeb668c48337bd668ada39beae65b0112877542d
Alastair McGowan-Douglas <alastair.douglas@gmail.com>
2013-07-08 22:39:45 +0100

    Tidy up script

--- parse.pl
+++ parse.pl
@@ -18,9 +18,12 @@ my @files = (shift) // File::Find::Rule->file()->name('*.pc')->in('pod');
 for my $file (@files) {
     my $html_fn = $file =~ s/pod/html/r =~ s/\.pc$/.html/r;
 
-    !-e $html_fn or (stat $file)[9] > (stat $html_fn)[9] or next;
+    if (!-e $html_fn or (stat $file)[9] > (stat $html_fn)[9]) {
+        print "Skipping $file\n";
+        next;
+    }
 
-    open my $html_out, ">", $html_fn or warn "Could not open $html_fn: $!" and next;
+    open my $html_out, ">", $html_fn or warn "Could not open $html_fn: $!", next;
     $pc->parse_file($file);
 
     $html_out->print($pc->{html}->as_html());
